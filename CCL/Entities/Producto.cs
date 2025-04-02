392d7631f17b7d61ea240acb6f2271cc9fa721e9
David Alvarado <davidalvarado0814@hotmail.com>
2025-04-01 23:45:26 -0500

    Feat: Añadir soporte para Entity Framework Core y PostgreSQL
    Se han agregado referencias a Entity Framework Core y Npgsql en `CCL.csproj` para habilitar la conexión a bases de datos PostgreSQL. Se ha creado el archivo `CclContext.cs` para definir el contexto de la base de datos y la entidad `Producto`.
    
    Se ha eliminado el controlador `WeatherForecastController.cs` y la clase `WeatherForecast.cs`, lo que indica que la funcionalidad de pronóstico del tiempo ya no está presente en el proyecto. Además, se ha actualizado `appsettings.json` para incluir la cadena de conexión a la base de datos.

---
+++ CCL/Entities/Producto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CCL.Entities;
+
+public partial class Producto
+{
+    public int Idproducto { get; set; }
+
+    public string Nombreproducto { get; set; } = null!;
+
+    public int Cantidadproducto { get; set; }
+}
