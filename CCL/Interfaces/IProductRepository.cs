b345fb4179d0a2ec59cf962ef5fde4afe05a694c
David Alvarado <davidalvarado0814@hotmail.com>
2025-04-02 00:12:06 -0500

    Eliminar carpeta Repositories y añadir repositorio de productos
    Se ha actualizado el archivo `CCL.csproj` para eliminar la carpeta `Repositories`.
    Se ha añadido la interfaz `IProductRepository` en `IProductRepository.cs`, que incluye métodos para obtener un producto por ID, actualizar su cantidad y obtener el inventario.
    Además, se ha implementado esta interfaz en la clase `ProductRepository` en `ProductRepository.cs`, que proporciona métodos asíncronos para interactuar con la base de datos a través del contexto `CclContext`.

---
+++ CCL/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using CCL.Entities;
+
+namespace CCL.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Producto?> ObtenerPorId(int idProducto);
+        Task<int> ActualizarCantidad(int idProducto, int cantidad);
+        List<Producto> ObtenerInventario();
+    }   
+}
---
+++ CCL/Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using CCL.Entities;
+using CCL.Interfaces;
+
+namespace CCL.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly CclContext _context;
+
+        public ProductRepository(CclContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Producto?> ObtenerPorId(int idProducto)
+        {
+            var productoDb = await _context.Productos.FindAsync(idProducto);
+            return productoDb ?? null;
+        }
+
+        public async Task<int> ActualizarCantidad(int idProducto, int cantidad)
+        {
+            var producto = await _context.Productos.FindAsync(idProducto);
+            if (producto != null)
+            {
+                producto.Cantidadproducto = cantidad;
+                return await _context.SaveChangesAsync();
+            }
+            else return 0;
+        }
+
+        public List<Producto> ObtenerInventario()
+        {
+            return _context.Productos.ToList();
+        }
+    }
+}
+}
