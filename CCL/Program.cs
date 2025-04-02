3335c649c29c16f682e3cc39ee99a237511ba95a
David Alvarado <davidalvarado0814@hotmail.com>
2025-04-01 22:46:01 -0500

    Agregar archivos de proyecto.

---
+++ CCL/Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("Policy", policy => {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseCors("Policy");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+app.Run();
